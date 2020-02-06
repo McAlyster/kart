758b5946e76c9604a5439a71c1097fa01b8010ce
Olivier <pressrock@free.fr>
2020-02-06 17:03:42 +0100

    DEBUG drf-haystack not tracked (~/.virtualenvs)

--- people/models.py
+++ people/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-
+from django.conf import settings
 from django_countries.fields import CountryField
 from languages.fields import LanguageField
 
@@ -44,7 +44,7 @@ class FresnoyProfile(models.Model):
     family_status = models.CharField(max_length=50,
                                      null=True, blank=True)
 
-    mother_tongue = LanguageField(blank=True, null=True)
+    mother_tongue = LanguageField(max_length=24, blank=True, null=True)
     other_language = models.CharField(max_length=24, null=True, blank=True)
 
     cursus = models.TextField(blank=True)
